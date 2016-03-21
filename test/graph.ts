215308989cb4d062e84034d6515dc4ad22c17721
Juan Pablo Garcia <juanpablogarcia@gmail.com>
2016-03-21 09:15:04 -0300

    Add identity test file

--- test/graph.ts
+++ test/graph.ts
@@ -1,20 +1,20 @@
 /// <reference path="../typings/main.d.ts" />
-import { expect } from 'chai';
-import { mock } from 'sinon';
-import { Graph } from '../src/graph';
+import { expect } from "chai";
+import { mock } from "sinon";
+import { Graph } from "../src/graph";
 
-describe('Graph', () => {
+describe("Graph", () => {
 
-    describe('meAsync', () => {
+    describe("meAsync", () => {
 
-        it('should get /me url', () => {
+        it("should get /me url", () => {
             var graph = new Graph({
-                defaultAccessToken: 'access_token'
+                defaultAccessToken: "access_token"
             });
 
             var mockedGet = mock(graph)
-                .expects('get')
-                .withArgs('https://graph.microsoft.com/v1.0/me/')
+                .expects("get")
+                .withArgs("https://graph.microsoft.com/v1.0/me/")
 
             graph.meAsync().then(() => {
                 mockedGet.verify();
---
+++ test/identity.ts
@@ -0,0 +1,10 @@
+/// <reference path="../typings/main.d.ts" />
+import { expect } from "chai";
+import { mock } from "sinon";
+import { Identity } from "../src/identity";
+
+describe("Identity", () => {
+
+    describe("constructor", () => {
+    });
+});
